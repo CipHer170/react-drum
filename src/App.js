eab7f7623cb90f95c029e289d5c3c12efd1a94ed
CipHer170 <105492413+CipHer170@users.noreply.github.com>
2022-09-13 11:54:56 +0500

    drum keys added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import soundQ from "./sounds/qkeyboard.wav";
+import sounds from "./sounds/sounds.js";
 
 function App() {
+  const start = (sound) => {
+    const keyboard = new Audio(sound);
+    keyboard.play();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="drumMain">
+        {/* drum buttons */}
+        <div className="drumButtons">
+          {sounds.map(({ key, sound }) => {
+            return (
+              <button key={key} onClick={start.bind(this, sound)}>
+                {key}
+              </button>
+            );
+          })}
+        </div>
+        {/* controls */}
+        <div className="remoteDrum">
+          <input type="range" min="1" max="2" className="onOff" />
+          <span className="volumeMain">
+            <p className="showVolumeValue">showVolumeValue </p>
+            <div className="manipulateVolume">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                className="manipulateVolume"
+              />
+            </div>
+          </span>
+          <div className="options">options</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/sounds/sounds.js
@@ -0,0 +1,21 @@
+import soundQ from "../sounds/qkeyboard.wav";
+import soundW from "../sounds/wkeyboard.wav";
+import soundE from "../sounds/ekeyboard.wav";
+import soundS from "../sounds/skeyboard.wav";
+import soundD from "../sounds/dkeyboard.wav";
+import soundZ from "../sounds/zkeyboard.wav";
+import soundX from "../sounds/xkeyboard.wav";
+import soundC from "../sounds/ckeyboard.wav";
+
+const sounds = [
+  { key: "q", sound: soundQ },
+  { key: "w", sound: soundW },
+  { key: "e", sound: soundE },
+  { key: "s", sound: soundS },
+  { key: "d", sound: soundD },
+  { key: "z", sound: soundZ },
+  { key: "x", sound: soundX },
+  { key: "c", sound: soundC },
+];
+
+export default sounds;
